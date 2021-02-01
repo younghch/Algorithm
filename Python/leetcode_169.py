4f198327149e8cb85bc6f235ee9079fe032f834c
ehehehe95 <53392103+ehehehe95@users.noreply.github.com>
2021-02-01 13:49:49 +0900

    Devine and Conquer : leetcode 169

---
+++ Python/leetcode_169.py
@@ -0,0 +1,27 @@
+"""
+Given an array nums of size n, return the majority element.
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+"""
+from typing import *
+import collections
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        counts = collections.defaultdict(int)
+        for num in nums:
+            if counts[num] == 0:
+                counts[num] = nums.count(num)
+            if counts[num] > len(nums) // 2:
+                return num
+
+    def majorityElement2(self, nums: List[int]) -> int:
+        if not nums:
+            return None
+        if len(nums) == 1:
+            return nums[0]
+
+        half = len(nums) // 2
+        a = self.majorityElement2(nums[:half])
+        b = self.majorityElement2(nums[half:])
+
+        return [b, a][nums.count(a) > half]
