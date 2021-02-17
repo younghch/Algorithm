b97eae9e39d86dfa82ad4d8704d15c6924f1f481
ehehehe95 <53392103+ehehehe95@users.noreply.github.com>
2021-02-17 16:21:59 +0900

    update : faster solution using floyd warshall

--- Python/kakao2021_TaxiRoute.py
+++ Python/kakao2021_TaxiRoute.py
@@ -61,4 +61,24 @@ def solution(n, s, a, b, fares):
     return answer
 
 
+def solution2(n, s, a, b, fares):
+    d = [[20000001 for _ in range(n)] for _ in range(n)]
+    for x in range(n):
+        d[x][x] = 0
+    for x, y, c in fares:
+        d[x-1][y-1] = c
+        d[y-1][x-1] = c
+
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                if d[j][k] > d[j][i] + d[i][k]:
+                    d[j][k] = d[j][i] + d[i][k]
+
+    minv = 40000002
+    for i in range(n):
+        minv = min(minv, d[s-1][i]+d[i][a-1]+d[i][b-1])
+    return minv
+
+
 print(solution(6, 4, 6, 2, 	[[4, 1, 10], [3, 5, 24], [5, 6, 2], [3, 1, 41], [5, 1, 24], [4, 6, 50], [2, 4, 66], [2, 3, 22], [1, 6, 25]]))
---
+++ Python/test_note.py
@@ -0,0 +1,2 @@
+d = [[201 for _ in range(2)] for _ in range(3)]
+print(d)
