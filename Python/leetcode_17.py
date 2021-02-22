6b0a59a2e313fcab5a893079f834f36bc797ce8b
ehehehe95 <53392103+ehehehe95@users.noreply.github.com>
2021-02-22 21:39:44 +0900

    add : simple dfs

---
+++ Python/leetcode_17.py
@@ -0,0 +1,28 @@
+"""
+Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+
+A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+"""
+from typing import *
+
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        def dfs(index, path):
+            if len(path) == len(digits):
+                result.append(path)
+                return
+
+            for i in range(index, len(digits)):
+                for j in dic[digits[i]]:
+                    dfs(i + 1, path + j)
+
+        if not digits:
+            return []
+
+        dic = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl",
+               "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
+        result = []
+        dfs(0, "")
+
+        return result
