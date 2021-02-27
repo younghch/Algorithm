cb5ae0b54077fb6bb49192696d972474cdef362b
ehehehe95 <53392103+ehehehe95@users.noreply.github.com>
2021-02-27 11:03:37 +0900

    add : simple string manipulation and sort

---
+++ Python/leetcode_937.py
@@ -0,0 +1,31 @@
+"""
+You are given an array of logs. Each log is a space-delimited string of words, where the first word is the identifier.
+
+There are two types of logs:
+
+Letter-logs: All words (except the identifier) consist of lowercase English letters.
+Digit-logs: All words (except the identifier) consist of digits.
+Reorder these logs so that:
+
+The letter-logs come before all digit-logs.
+The letter-logs are sorted lexicographically by their contents. If their contents are the same, then sort them lexicographically by their identifiers.
+The digit-logs maintain their relative ordering.
+Return the final order of the logs.
+"""
+
+from typing import *
+
+
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        digits = list()
+        letters = list()
+
+        for log in logs:
+            if log.split()[1].isdigit():
+                digits.append(log)
+            else:
+                letters.append(log)
+        letters.sort(key=lambda x: (x.split()[1:], x.split()[0]))
+
+        return letters + digits
