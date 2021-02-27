e0944938c667ee49a418648eb2d08445b89c860e
ehehehe95 <53392103+ehehehe95@users.noreply.github.com>
2021-02-27 11:36:52 +0900

    update : better and faster solution using similar method

--- Python/leetcode_49.py
+++ Python/leetcode_49.py
@@ -9,7 +9,7 @@ import collections
 
 
 class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+    def groupAnagrams1(self, strs: List[str]) -> List[List[str]]:
         words = collections.defaultdict(list)
 
         # make w1, w2 always be sorted
@@ -33,6 +33,13 @@ class Solution:
                 words[sorted_w].append(word)
         return words.values()
 
+    def groupAnagrams2(self, strs: List[str]) -> List[List[str]]:
+        anagrams = collections.defaultdict(list)
+
+        for word in strs:
+            anagrams[''.join(sorted(word))].append(word)
+        return anagrams.values()
+
 
 s = Solution()
-print(s.groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
+print(s.groupAnagrams1(["eat", "tea", "tan", "ate", "nat", "bat"]))
