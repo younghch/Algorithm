6bcf4c1018edceb09a145cf407e3faa5548a82e9
ehehehe95 <53392103+ehehehe95@users.noreply.github.com>
2021-03-16 14:26:16 +0900

    end : graph search with dfs
    be careful for the initial condition

--- Python/Baekjoon/11724_count_connected.py
+++ Python/Baekjoon/11724_count_connected.py
@@ -5,6 +5,8 @@
 import collections
 import sys
 
+sys.setrecursionlimit(10**6)
+
 # n = number of vertex, m = number of line
 n, m = map(int, sys.stdin.readline().split())
 graph = collections.defaultdict(list)
@@ -13,4 +15,29 @@ for i in range(m):
     graph[a].append(b)
     graph[b].append(a)
 
-# idea : dfs?
+# idea : dfs
+been = [0 for _ in range(n)]
+been.append(0)
+been[0] = 1
+
+
+def dfs(v):
+    next_l = graph[v]
+    for next_v in next_l:
+        if been[next_v] == 1:
+            continue
+        been[next_v] = 1
+        dfs(next_v)
+
+
+ans = 0
+idx = 0
+while idx != -1:
+    try:
+        idx = been.index(0)
+        ans += 1
+        been[idx] = 1
+        dfs(idx)
+    except ValueError:
+        idx = -1
+print(ans)
--- Python/test_note.py
+++ Python/test_note.py
@@ -1,3 +1,16 @@
 import collections
+test = [1, 2, 3, 4]
 
-print(-1 % 4)
+
+def rec():
+    for i in range(4):
+        if test[i] == 4:
+            print(i, " is four!!")
+            continue
+        else:
+            test[i] += 1
+            print("rec", i)
+            rec()
+
+rec()
+
