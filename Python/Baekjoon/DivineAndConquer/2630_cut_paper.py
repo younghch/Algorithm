b090480df4d4b549ec6603c50cd39de76bbf0331
Young <younghch42@gmail.com>
2022-02-25 18:28:49 +0900

    Add blank line

--- Python/Baekjoon/DivineAndConquer/2630_cut_paper.py
+++ Python/Baekjoon/DivineAndConquer/2630_cut_paper.py
@@ -28,6 +28,7 @@ def divide_if_not_square(x, y, l):
     l = l//2
     next_xs = (x, x+l)
     next_ys = (y, y+l)
+    
     for next_x in next_xs:
         for next_y in next_ys:
             if not check_and_add(next_x, next_y, l):
