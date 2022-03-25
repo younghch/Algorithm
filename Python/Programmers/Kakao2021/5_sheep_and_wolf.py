91e3dbc427a94e262c35ed0121becc67bebd2476
Young <younghch42@gmail.com>
2022-03-26 00:03:42 +0900

    Add dfs with condition

---
+++ Python/Programmers/Kakao2021/5_sheep_and_wolf.py
@@ -0,0 +1,29 @@
+'''
+
+'''
+from collections import defaultdict
+import sys
+
+sys.setrecursionlimit(10**9)
+
+def solution(info, edges):
+    global max_num_of_sheep
+    
+    max_num_of_sheep = 0
+    tree = defaultdict(list)
+    for parent, child in edges:
+        tree[parent].append(child)
+    
+    def dfs(cur, candidates, count_sheep, count_wolf):
+        global max_num_of_sheep
+        count_sheep += info[cur]^1
+        count_wolf += info[cur]
+        if count_sheep > count_wolf:
+            if count_sheep > max_num_of_sheep:
+                max_num_of_sheep = count_sheep
+            next_candidates = candidates + tree[cur]
+            for next_ in next_candidates:
+                dfs(next_, [x for x in next_candidates if x != next_],count_sheep, count_wolf)
+
+    dfs(0, [], 0, 0)
+    return max_num_of_sheep
