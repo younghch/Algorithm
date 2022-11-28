d1c1423df3cdb0bfc94151597b64d3e43aca9c46
Young <younghch42@gmail.com>
2022-11-28 10:41:18 +0900

    Format code

--- Kotlin/src/test/kotlin/leetcode/TwoSumTest.kt
+++ Kotlin/src/test/kotlin/leetcode/TwoSumTest.kt
@@ -10,19 +10,18 @@ internal class TwoSumTest {
 
     @Test
     fun twoSum1() {
-        Assertions.assertArrayEquals(intArrayOf(0,1), solution.twoSum(intArrayOf(2,7,11,15), 9))
+        Assertions.assertArrayEquals(intArrayOf(0, 1), solution.twoSum(intArrayOf(2, 7, 11, 15), 9))
     }
 
     @Test
     fun twoSum2() {
-        Assertions.assertArrayEquals(intArrayOf(1,2), solution.twoSum(intArrayOf(3,2,4), 6))
+        Assertions.assertArrayEquals(intArrayOf(1, 2), solution.twoSum(intArrayOf(3, 2, 4), 6))
     }
 
     @Test
     fun twoSum3() {
-        Assertions.assertArrayEquals(intArrayOf(0,1), solution.twoSum(intArrayOf(3,3), 6))
+        Assertions.assertArrayEquals(intArrayOf(0, 1), solution.twoSum(intArrayOf(3, 3), 6))
     }
-
 
 
 }
