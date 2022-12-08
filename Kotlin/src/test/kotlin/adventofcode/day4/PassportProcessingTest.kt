1a0ff461081429c4380fb1a7c7ec3b5a30337975
Young <younghch42@gmail.com>
2022-12-08 11:03:24 +0900

    Add test for count function, success all test

--- Kotlin/src/test/kotlin/adventofcode/day4/PassportProcessingTest.kt
+++ Kotlin/src/test/kotlin/adventofcode/day4/PassportProcessingTest.kt
@@ -22,7 +22,6 @@ internal class PassportProcessingTest {
     """.trimIndent()
 
     @Test
-
     fun putLineInfoToPassportSingle() {
         val line = "hgt:179cm"
 
@@ -111,13 +110,30 @@ internal class PassportProcessingTest {
 
     @Test
     fun countNumOfValid() {
-        TODO()
+        val lines = listOf(
+            "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd",
+            "byr:1937 iyr:2017 cid:147 hgt:183cm",
+            "",
+            "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884",
+            "hcl:#cfa07d byr:1929",
+            "",
+            "hcl:#ae17e1 iyr:2013",
+            "eyr:2024",
+            "ecl:brn pid:760753108 byr:1931",
+            "hgt:179cm",
+            "",
+            "hcl:#cfa07d eyr:2025 pid:166559648",
+            "iyr:2011 ecl:brn hgt:59in"
+        )
+        val countValid = PassportProcessing.countNumOfValid(lines)
+        Assertions.assertThat(countValid).isEqualTo(2)
     }
 
     @Test
     fun countNumOfValidFromInput() {
-        InputUtil.getListFromString(s) { it }
-        TODO()
+        val lines = InputUtil.getListFromString(s) { it }
+        val countValid = PassportProcessing.countNumOfValid(lines)
+        Assertions.assertThat(countValid).isEqualTo(2)
     }
 
 }
