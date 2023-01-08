9cf44e8312f82132f787b25d4aedd7e97805b552
Young <younghch42@gmail.com>
2023-01-09 08:55:34 +0900

    Add solution of part2

--- Kotlin/src/main/kotlin/adventofcode/day8/Main.kt
+++ Kotlin/src/main/kotlin/adventofcode/day8/Main.kt
@@ -4,24 +4,65 @@ import adventofcode.util.InputUtil
 
 fun main(args: Array<String>) {
     val input = InputUtil.getListFromFile(args[0]) {
-        it.split(' ')
+        val line = it.split(' ')
+        Pair(line[0], line[1].toInt())
     }
-    val visited = mutableSetOf<Int>()
-    var cur = 0
-    var acc = 0
+    val sol1 = getIsInfiniteAndAcc(input)
+    val sol2 = tryChangeGetAcc(input)
+    println(sol1.second)
+    println(sol2)
+}
 
-    while (!visited.contains(cur)) {
+fun getIsInfiniteAndAcc(
+    instructions: List<Pair<String, Int>>,
+    curPointer: Int = 0,
+    accumulated: Int = 0
+): Pair<Boolean, Int> {
+    var acc = accumulated
+    var cur = curPointer
+    val visited = mutableSetOf<Int>()
+
+    while (!visited.contains(cur) && cur < instructions.size) {
         visited.add(cur)
-        val (operation, value) = input[cur]
+        val (operation, value) = instructions[cur]
         when (operation) {
             "nop" -> cur++
             "acc" -> {
                 cur++
-                acc += value.toInt()
+                acc += value
             }
 
-            "jmp" -> cur += value.toInt()
+            "jmp" -> cur += value
         }
     }
-    println(acc)
+    return Pair(cur < instructions.size, acc)
+}
+
+fun tryChangeGetAcc(instructions: List<Pair<String, Int>>): Int {
+    var acc = 0
+    var cur = 0
+    var tried = Pair(true, 0)
+
+    while (tried.first) {
+        val (operation, value) = instructions[cur]
+        when (operation) {
+            "nop" -> {
+                tried = getIsInfiniteAndAcc(instructions, cur + value, acc)
+                if (tried.first)
+                    cur++
+            }
+
+            "acc" -> {
+                cur++
+                acc += value
+            }
+
+            "jmp" -> {
+                tried = getIsInfiniteAndAcc(instructions, cur + 1, acc)
+                if (tried.first)
+                    cur += value
+            }
+        }
+    }
+    return tried.second
 }
