84ba171cb84d3a4229c19d7752526ddd8977df29
Young <younghch42@gmail.com>
2024-02-23 20:14:01 +0900

    refactor: clean code

--- Python/Baekjoon/DynamicProgramming/LV3/1311_job_allocation.py
+++ Python/Baekjoon/DynamicProgramming/LV3/1311_job_allocation.py
@@ -4,6 +4,17 @@ import sys
 
 input_ = sys.stdin.readline
 
+def get_minimum_effort(n, effort):
+    dp = [float('inf')]*(1<<n)
+    dp[0] = 0
+    for path in range(1<<n):
+        num_participated = count_ones(path)
+        for work_idx in range(n):
+            mask = 1<<work_idx
+            if (not path&mask):
+                dp[path|mask] = min(dp[path|mask], dp[path]+effort[num_participated][work_idx])
+    return dp[-1]
+
 def count_ones(n):
     count = 0
     while n:
@@ -12,16 +23,11 @@ def count_ones(n):
         n>>=1
     return count
 
-n = int(input_())
-effort = [list(map(int, input_().split())) for _ in range(n)]
-dp = [float('inf')]*(1<<n)
-dp[0] = 0
+def main():
+    n = int(input_())
+    effort = [list(map(int, input_().split())) for _ in range(n)]
+    print(get_minimum_effort(n, effort))
 
-for path in range(1<<n):
-    num_participated = count_ones(path)
-    for work_idx in range(n):
-        mask = 1<<work_idx
-        if (not path&mask):
-            dp[path|mask] = min(dp[path|mask], dp[path]+effort[num_participated][work_idx])
 
-print(dp[-1])
+if __name__ == '__main__':
+    main()
