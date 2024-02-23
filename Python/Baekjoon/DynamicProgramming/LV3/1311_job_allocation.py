7c40f198becb9296a38982717f31f3edc7d5e73f
Young <younghch42@gmail.com>
2024-02-23 19:53:38 +0900

    refactor: delete unnecessary list copy

--- Python/Baekjoon/DynamicProgramming/LV3/1311_job_allocation.py
+++ Python/Baekjoon/DynamicProgramming/LV3/1311_job_allocation.py
@@ -4,17 +4,25 @@ import sys
 
 input_ = sys.stdin.readline
 
+def count_ones(n):
+    count = 0
+    while n:
+        if (n&1):
+            count += 1
+        n>>=1
+    return count
+
 n = int(input_())
 effort = [list(map(int, input_().split())) for _ in range(n)]
-dp = [float('inf')]*(2**n)
+dp = [float('inf')]*(1<<n)
 dp[0] = 0
 
 for person in range(n):
-    before_dp = [v for v in dp]
-    for work_idx in range(n):
-        mask = 1<<work_idx
-        for visited in range(2**n):
-            if visited&mask:
-                continue
-            dp[visited|mask] = min(before_dp[visited]+effort[person][work_idx], dp[visited|mask])
+    for path in range(1<<n):
+        if count_ones(path) == person:
+            for i in range(n):
+                mask = 1<<i
+                if (not path&mask):
+                    dp[path|mask] = min(dp[path|mask], dp[path]+effort[person][i])
+
 print(dp[-1])
