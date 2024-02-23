c25a0692bed047e9c90dbbeee6ee637d61578879
Young <younghch42@gmail.com>
2024-02-23 20:10:35 +0900

    refactor: remove outer for loop and get participants information from the bit

--- Python/Baekjoon/DynamicProgramming/LV3/1311_job_allocation.py
+++ Python/Baekjoon/DynamicProgramming/LV3/1311_job_allocation.py
@@ -17,12 +17,11 @@ effort = [list(map(int, input_().split())) for _ in range(n)]
 dp = [float('inf')]*(1<<n)
 dp[0] = 0
 
-for person in range(n):
-    for path in range(1<<n):
-        if count_ones(path) == person:
-            for i in range(n):
-                mask = 1<<i
-                if (not path&mask):
-                    dp[path|mask] = min(dp[path|mask], dp[path]+effort[person][i])
+for path in range(1<<n):
+    num_participated = count_ones(path)
+    for work_idx in range(n):
+        mask = 1<<work_idx
+        if (not path&mask):
+            dp[path|mask] = min(dp[path|mask], dp[path]+effort[num_participated][work_idx])
 
 print(dp[-1])
