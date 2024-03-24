3876ba0106be3050bc9bc2ff60b649065515d23d
Young <younghch42@gmail.com>
2024-03-24 23:04:44 +0900

    feature: add kandane's algorithm solution for leetcode maximum subarray question

---
+++ Python/Leetcode/maximum_subarray.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/maximum-subarray/description/?envType=study-plan-v2&envId=top-interview-150
+from typing import List
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        sum_max = nums[0]
+        sum_current = nums[0]
+        for i in range(1, len(nums)):
+            num = nums[i]
+            sum_current = max(sum_current+num, num)
+            sum_max = max(sum_current, max)
+        return sum_max
