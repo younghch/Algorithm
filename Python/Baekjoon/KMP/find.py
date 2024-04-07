edb882ed778f3aa11255045a65f76ed340d74b48
Young <younghch42@gmail.com>
2024-04-07 15:24:52 +0900

    refactor: clean code

--- Python/Baekjoon/KMP/find.py
+++ Python/Baekjoon/KMP/find.py
@@ -2,24 +2,13 @@
 
 import sys
 
-input_ = sys.stdin.readline
-
-sentence = input_()[:-1]
-word = input_()[:-1]
-
 def calculate_list_len_prefix_equals_suffix(word: str):
     ps_equals = [0]
     for i in range(1, len(word)):
         cand = ps_equals[i-1]
-        while True:
-            if word[cand] == word[i]:
-                ps_equals.append(cand+1)
-                break
-            else:
-                if cand == 0:
-                    ps_equals.append(0)
-                    break
-                cand = ps_equals[cand-1]
+        while cand > 0 and word[cand] != word[i]:
+            cand = ps_equals[cand-1]
+        ps_equals.append(cand+1 if word[cand] == word[i] else cand)
     return ps_equals
 
 def kmp(sentence: str, word: str):
@@ -27,8 +16,11 @@ def kmp(sentence: str, word: str):
     ps_equals = calculate_list_len_prefix_equals_suffix(word)
     m = 0
     s = 0
-    while m < len(sentence) - len(word) + 1:
-        while s < len(word) and m < len(sentence) - len(word) + 1:
+
+    max_m = len(sentence) - len(word) + 1
+
+    while m < max_m:
+        while s < len(word) and m < max_m:
             if sentence[m+s] != word[s]:
                 if s == 0:
                     m = m+1
@@ -44,6 +36,14 @@ def kmp(sentence: str, word: str):
     
     return occurence_idx
 
-occurences = kmp(sentence, word)
-print(len(occurences))
-print(str.join(' ', map(lambda x: str(x+1), occurences)))
+def main():
+    input_ = sys.stdin.readline
+
+    sentence = input_()[:-1]
+    word = input_()[:-1]
+
+    occurences = kmp(sentence, word)
+    print(len(occurences))
+    print(str.join(' ', map(lambda x: str(x+1), occurences)))
+
+main()
