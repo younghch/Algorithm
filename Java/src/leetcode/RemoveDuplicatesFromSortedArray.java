214f9abacd63e67626fd0207ee20845766d6d9e5
Young <younghch42@gmail.com>
2024-05-03 20:22:03 +0900

    refactor: add better solution

--- Java/src/leetcode/RemoveDuplicatesFromSortedArray.java
+++ Java/src/leetcode/RemoveDuplicatesFromSortedArray.java
@@ -6,8 +6,8 @@ public class RemoveDuplicatesFromSortedArray {
     public int removeDuplicates(int[] nums) {
         boolean wasSame = false;
         int curIdx = 1;
-        for (int i=1; i < nums.length; i++) {
-            boolean isSame = nums[i] == nums[i-1];
+        for (int i = 1; i < nums.length; i++) {
+            boolean isSame = nums[i] == nums[i - 1];
             if (!(isSame & wasSame)) {
                 nums[curIdx] = nums[i];
                 curIdx++;
@@ -15,5 +15,17 @@ public class RemoveDuplicatesFromSortedArray {
             wasSame = isSame;
         }
         return curIdx;
+    }
+
+    public int removeDuplicatesBetterSolution(int[] nums) {
+        int i = 2;
+        for (int j = 2; j < nums.length; j++) {
+            int num = nums[j];
+            if (nums[i - 2] != num) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
     }
 }
