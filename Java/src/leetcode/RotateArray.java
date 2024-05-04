4ea3ec041fce3517d27c3e804a67d8600ed3fc12
Young <younghch42@gmail.com>
2024-05-04 12:32:11 +0900

    feature: add solution without using additional space

--- Java/src/leetcode/RotateArray.java
+++ Java/src/leetcode/RotateArray.java
@@ -3,14 +3,31 @@ package leetcode;
 // https://leetcode.com/problems/rotate-array
 public class RotateArray {
 
+    public void rotate(int[] nums, int k) {
+        int rotate = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, rotate - 1);
+        reverse(nums, rotate, nums.length - 1);
+    }
+
+    void reverse(int[] nums, int startIdx, int endIdx) {
+        while (startIdx < endIdx) {
+            int temp = nums[startIdx];
+            nums[startIdx] = nums[endIdx];
+            nums[endIdx] = temp;
+            startIdx++;
+            endIdx--;
+        }
+    }
+
     public void rotateCheckMoved(int[] nums, int k) {
         boolean[] moved = new boolean[nums.length];
-        for (int i=0; i<nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
             int curIdx = i;
             int curValue = nums[i];
             while (!moved[curIdx]) {
                 moved[curIdx] = true;
-                int rotatedIdx = (curIdx+k)%nums.length;
+                int rotatedIdx = (curIdx + k) % nums.length;
                 int rotatedValue = nums[rotatedIdx];
                 nums[rotatedIdx] = curValue;
                 curIdx = rotatedIdx;
@@ -23,15 +40,15 @@ public class RotateArray {
         int rotate = k % nums.length;
         int[] rotatedNums = new int[nums.length];
         int j = 0;
-        for (int i=nums.length - rotate; i<nums.length; i++) {
+        for (int i = nums.length - rotate; i < nums.length; i++) {
             rotatedNums[j] = nums[i];
             j++;
         }
-        for (int i=0; i<nums.length - rotate; i++) {
+        for (int i = 0; i < nums.length - rotate; i++) {
             rotatedNums[j] = nums[i];
             j++;
         }
-        for (int l=0; l<nums.length; l++) {
+        for (int l = 0; l < nums.length; l++) {
             nums[l] = rotatedNums[l];
         }
     }
