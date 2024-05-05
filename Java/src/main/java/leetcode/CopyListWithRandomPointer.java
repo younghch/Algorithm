454801812a1c1c2245d9200b0fb654d7dd590996
Young <younghch42@gmail.com>
2024-05-05 13:22:05 +0900

    feature: add solution to copy list with random pointer

---
+++ Java/src/main/java/leetcode/CopyListWithRandomPointer.java
@@ -0,0 +1,42 @@
+package leetcode;
+
+import java.util.HashMap;
+
+public class CopyListWithRandomPointer {
+    public Node copyRandomList(Node head) {
+        if (head == null) return null;
+        HashMap<Node, Node> nodeReference = new HashMap<>();
+
+
+        Node current = head;
+        while (current != null) {
+            Node copied = new Node(current.val);
+            nodeReference.put(current, copied);
+            current = current.next;
+        }
+
+        current = head;
+        while (current != null) {
+            Node copied = nodeReference.get(current);
+            copied.next = nodeReference.get(current.next);
+            copied.random = nodeReference.get(current.random);
+            current = current.next;
+        }
+
+        return nodeReference.get(head);
+
+
+    }
+}
+
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
