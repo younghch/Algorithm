f100116dbdd05339f43a572c55b654b1a2614217
Young <younghch42@gmail.com>
2024-05-16 11:01:27 +0900

    refactor: store index instead of the object to save memory

--- Python/Leetcode/739_daily_temperatures.py
+++ Python/Leetcode/739_daily_temperatures.py
@@ -4,17 +4,13 @@ from typing import List
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         answer = [0] * len(temperatures)
-        stack: List[Temperature] = list()
+        stack: List = list()
         for i in range(len(temperatures)):
             temperature = temperatures[i]
-            while len(stack) != 0 and stack[-1].temperature < temperature:
-                past_record = stack.pop()
-                answer[past_record.idx] = i - past_record.idx
-            stack.append(Temperature(temperature, i))
+            while stack and temperatures[stack[-1]] < temperature:
+                idx = stack.pop()
+                answer[idx] = i - idx
+            stack.append(i)
         return answer
 
-class Temperature:
-    def __init__(self, temperature, idx) -> None:
-        self.temperature = temperature;
-        self.idx = idx
 
